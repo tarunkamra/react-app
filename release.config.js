ba701e02deb6054aeffbbffa28908c8874f61aaf
Tarun Kamra <tarunkamra18@gmail.com>
2022-02-27 11:17:23 +0530

    fix: fix description

---
+++ release.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+
+"branches": "main",
+
+repositoryUrl: "https://github.com/tarunkamra/react-app",
+
+plugins: ['@semantic-release/commit-analyzer', '@semantic-release/release-notes-generator', '@semantic-release/github']
+
+
+};
